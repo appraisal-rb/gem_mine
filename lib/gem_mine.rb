ee8f9f471c0f1aa5bdb950b17de9f07cdcd25402
Peter H. Boling <peter.boling@gmail.com>
2025-08-30 22:29:09 -0600

    🎉 Initial commit

---
+++ lib/gem_mine.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "gem_mine/version"
+
+module GemMine
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/gem_mine/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module GemMine
+  VERSION = "0.1.0"
+end
